4c723cc364ce48ae65f5b1dde731285f8779a6b0
Bsmart21 <bst921.0@gmail.com>
2025-03-30 13:57:29 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function loadDropdownOptions(options, dropdownId) {
 
 // Function to send the selected option to the Apps Script
 function updateSheet(sheetName, selectedValue) {
-    const url = `https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?sheet=${sheetName}&value=${encodeURIComponent(selectedValue)}`;
+    const url = `https://script.google.com/macros/s/AKfycbyNfd81XsCjlzbDVEvEp_gHra-340lOJ_Um5o7nEjedmtlSeuBpweIyBqlQn0FwMpc4/exec?sheet=${sheetName}&value=${encodeURIComponent(selectedValue)}`;
 
     fetch(url)
         .then(response => response.json())
